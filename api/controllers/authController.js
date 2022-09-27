9f79d84ba4105bf0ed7fc1665e34234e6eb033cc
servo pcerda <fernandonando98@live.com.mx>
2022-09-26 19:28:38 -0500

    login

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,7 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jwt-simple';
 import { User } from '../models/index.js';
+import config from '../../config/index.js';
 
 const register = async (req, res) => {
   try {
@@ -27,6 +29,67 @@ const register = async (req, res) => {
   }
 };
 
-const login = () => {};
+const login = async (req, res) => {
+  /**
+   * 1.- Buscar usuario con ese correo
+   * 2.- Si no, respondemos con 401 credenciales inválidas
+   * 3.- Si encuentra usuario, comparar contraseñas (bcrypt.compare)
+   * 4.- Si no coinciden las pass 401 credenciales inválidas
+   * 5.- Si sí coinciden generemos un token y se lo mandamos
+   */
+
+  /**
+   * req.body = {
+   *  password:
+   *  email:
+   * }
+   */
+
+  try {
+    //Buscar usuario por correo
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+
+    //VAlidar que existas usuario
+    if (!user) {
+      return res.status(401).json({
+        msg: 'Credenciales inválidas',
+      });
+    }
+
+    //Comparamos contraseñas
+    const passwordMatched = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    //Si no coinciden retornamos 401
+    if (!passwordMatched) {
+      return res.status(401).json({
+        msg: 'Credenciales inválidas',
+      });
+    }
+
+    //ACÁ GENERAR TOKEN Y RETORNARLO
+
+    //objeto a guardar en el token
+    const payload = {
+      userId: user.id,
+    };
+
+    const token = jwt.encode(payload, config.token.secret);
+
+    return res.json({
+      msg: 'Login satisfactorio',
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: 'Error al hacer login',
+      error,
+    });
+  }
+};
 
 export { register, login };
